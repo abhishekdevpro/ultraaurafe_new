5892b21a1deead97178c63473c705e92c1b2698b
harshtrivedi01 <harsh2trivedi786@gmail.com>
2024-09-24 22:30:43 +0530

    update

--- src/components/pages/course/courseDetails/detailsContent.jsx
+++ src/components/pages/course/courseDetails/detailsContent.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {  useParams } from 'react-router-dom';
-import { propTypes } from "prop-types";
+import { PropTypes } from "prop-types";
 // import FeatherIcon from "feather-icons-react";
 
 import InstructorCard from "./InstructorCard";
