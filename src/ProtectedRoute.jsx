d86ca76267a8a52cdd7f749e87b031015e346938
Rishi kashyap <kashyap1strishi@gmail.com>
2024-10-04 14:31:07 +0530

    All little little updates done

---
+++ src/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+// This checks if the user is authenticated
+const ProtectedRoute = () => {
+  const isAuthenticated = !!localStorage.getItem('token') || localStorage.getItem('admintoken') || localStorage.getItem('vendorToken') || localStorage.getItem('trainerToken'); // Check for token
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
